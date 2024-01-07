3901a67c74155214806a3ce50c525dcd18517a36
ChoyMH23 <choymh23@gmail.com>
2024-01-07 09:09:22 +0800

    solving mobile device problems

--- src/components/Hero.jsx
+++ src/components/Hero.jsx
@@ -16,7 +16,8 @@ const Hero = () => {
 
         <div>
           <h1 className={`${styles.heroHeadText} text-white`}>
-            Hi, I'm <span className="text-[#915EFF]">Choy Min Han</span>
+            Hi, I'm <br className="sm:hidden" />
+            <span className="text-[#915EFF]">Choy Min Han</span>
           </h1>
           <p className={`${styles.heroSubText} mt-2 text-white-100`}>
             I am a digital maker who <br className="sm:block hidden" />
--- src/components/canvas/Computers.jsx
+++ src/components/canvas/Computers.jsx
@@ -52,14 +52,18 @@ const ComputersCanvas = () => {
       camera={{ position: [20, 3, 5], fov: 25 }}
       gl={{ preserveDrawingBuffer: true }}
     >
-      <Suspense fallback={<CanvasLoader />}>
-        <OrbitControls
-          enableZoom={false}
-          maxPolarAngle={Math.PI / 2}
-          minPolarAngle={Math.PI / 2}
-        />
+      {isMobile ? (
         <Computers isMobile={isMobile} />
-      </Suspense>
+      ) : (
+        <Suspense fallback={<CanvasLoader />}>
+          <OrbitControls
+            enableZoom={false}
+            maxPolarAngle={Math.PI / 2}
+            minPolarAngle={Math.PI / 2}
+          />
+          <Computers isMobile={isMobile} />
+        </Suspense>
+      )}
 
       <Preload all />
     </Canvas>
